653a95f6898183b411f055f48a474e7a4d8aec75
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2017-07-15 02:24:45 +0200

    Experimental with lvmetad Unix socket

--- cmd/dmsetup.go
+++ cmd/dmsetup.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/dswarbrick/devmapper"
@@ -33,4 +34,42 @@ func main() {
 
 		fmt.Printf("%#v %#v\n", device, targets)
 	}
+
+	// Experimental interaction with lvmetad Unix socket
+	conn, err := net.DialUnix("unix", nil, &net.UnixAddr{"/run/lvm/lvmetad.socket", "unix"})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	var buf [2048]byte
+	var n int
+
+	fmt.Printf("\nlvmetad socket: %#v\n", conn)
+
+	conn.Write([]byte("request = \"hello\"\n"))
+	conn.Write([]byte("\n##\n"))
+
+	n, _ = conn.Read(buf[:])
+	fmt.Printf("%s\n", buf[:n])
+
+	conn.Write([]byte("request=\"get_global_info\"\ntoken = \"skip\"\n"))
+	conn.Write([]byte("\n##\n"))
+
+	n, _ = conn.Read(buf[:])
+	fmt.Printf("%s\n", buf[:n])
+
+	conn.Write([]byte("request=\"vg_list\"\ntoken =\"filter:3239235440\"\n"))
+	conn.Write([]byte("\n##\n"))
+
+	n, _ = conn.Read(buf[:])
+	fmt.Printf("%s\n", buf[:n])
+
+	conn.Write([]byte("request=\"vg_lookup\"\nuuid =\"VIn7Hm-7z8y-AMFJ-6CdJ-6la7-dPd7-h1I6eO\"\ntoken =\"filter:3239235440\"\n"))
+	conn.Write([]byte("\n##\n"))
+
+	n, _ = conn.Read(buf[:])
+	fmt.Printf("%s\n", buf[:n])
 }
